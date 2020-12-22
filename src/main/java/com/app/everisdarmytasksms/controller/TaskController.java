062997d95bc254fbad4907a426aebfcbb38c6a3b
alfredo <alfredomontillaperez@gmail.com>
2020-12-22 15:26:40 +0100

    Modificacion CORS TaskController

--- src/main/java/com/app/everisdarmytasksms/controller/TaskController.java
+++ src/main/java/com/app/everisdarmytasksms/controller/TaskController.java
@@ -3,6 +3,7 @@ package com.app.everisdarmytasksms.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.app.everisdarmytasksms.model.Task;
 import com.app.everisdarmytasksms.service.TaskService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class TaskController {
 	
 	@Autowired
